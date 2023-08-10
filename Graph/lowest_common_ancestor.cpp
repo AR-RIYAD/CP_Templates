6157b22e76ea55f8eaa65eace98df004540d2574
AR-RIYAD <134699041+AR-RIYAD@users.noreply.github.com>
2023-08-10 12:23:34 +0600

    Update lowest_common_ancestor.cpp

--- Graph/lowest_common_ancestor.cpp
+++ Graph/lowest_common_ancestor.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 vector<vector<int>> tree;
 vector<int> parent;
-void bfs(int node,int parent=-1)
+void dfs(int node,int par=-1)
 {
-    parent[node]=parent;
+    parent[node]=par;
     for(auto child:tree[node])
     {
-        if(child!=parent) dfs(child,node);
+        if(child!=par) dfs(child,node);
     }
+}
+vector<int> path(int node)
+{
+    vector<int> res;
+    while(node!=-1)
+    {
+        res.push_back(node);
+        node=parent[node];
+    }
+    reverse(res.begin(),res.end());
+    return res;
 }
 int main()
 {
@@ -16,7 +27,7 @@ int main()
     cout<<"Enter the number of nodes in the tree:";
     cin>>n;
     tree.resize(n+1);
-    parent.resize(n+1)
+    parent.resize(n+1);
     for(int i=0;i<n-1;i++)
     {
         int x,y;
@@ -29,4 +40,21 @@ int main()
     int root;
     cin>>root;
     dfs(root);
+    int x,y;
+    cout<<"Enter two nodes which lca to be determines:";
+    cin>>x>>y;
+    vector<int> pathx=path(x);
+    vector<int> pathy=path(y);
+    int lca=-1,mn_length=min(pathx.size(),pathy.size());
+    //cout<<mn_length<<endl;
+    for(int i=0;i<mn_length;i++)
+    {
+        if(pathx[i]==pathy[i]) lca=pathx[i];
+        else break;
+    }
+    cout<<lca<<endl;
+    
+    return 0;
+    
+
 }
